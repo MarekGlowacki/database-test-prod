a1e8840b049285b31ffa17943c02502cbd44fab5
User <user@its.me>
2023-01-08 07:09:35 +0100

    Initial commit

---
+++ src/main/java/online/javafun/shop/DatabaseTestProdApplication.java
@@ -0,0 +1,13 @@
+package online.javafun.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseTestProdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseTestProdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/online/javafun/shop/DatabaseTestProdApplicationTests.java
@@ -0,0 +1,13 @@
+package online.javafun.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseTestProdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
